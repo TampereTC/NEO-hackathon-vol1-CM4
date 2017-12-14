c1dc4453caf16644b75b871c1472f5607d687a7b
Jarkko Puska <jarkko.puska@nokia.com>
2017-12-14 12:12:09 +0200

    Fixing app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,23 +1,20 @@
 const http = require('http');
 const fs = require('fs');
-// const hostname = '10.144.83.245';
-const hostname = 'add.your.ip.here';
 const port = 3000;
 
 fs.readFile('index.html', (err, html) => {
-		if(err){
-			throw err;
-		}
+	if(err) {
+		throw err;
+	}
 
 	const server = http.createServer((req, res) => {
 		res.statusCode = 200;
-		// res.setHeader('content-type', 'text/plain');
 		res.setHeader('content-type', 'text/html');
 		res.write(html);
 		res.end();
 	});
 	
-	server.listen(port, hostname, () => {
+	server.listen(port, () => {
 		console.log('Server start on port ' + port);
 	});		
 });
